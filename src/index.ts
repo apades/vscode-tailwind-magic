8c03eb3a0f8609060259bf75d07608649af5ae3b
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-12 18:14:54 +0800

    chore: add rules

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import fsp from 'node:fs/promises'
 import { addEventListener } from '@vscode-use/utils'
 import * as vscode from 'vscode'
 
+// todo: 在底部栏增加一个开关来控制是否要启动此插件进行自动处理
 const rules = [
   [/-\[\s*(rgb[a]\([^\)]*\))\s*\]/g, (_: string, v: string) => `-[${v.replace(/\s*/g, '')}]`],
   [/-\[\s*(calc\([^\)]*\))\s*\]/g, (_: string, v: string) => `-[${v.replace(/\s*/g, '')}]`],
+  [/-(\#[^\s\"]+)/g, (_: string, v: string) => `-[${v}]`],
+  [/-([0-9]+((px)|(vw)|(vh)|(rem)|(em)|%))/g, (_: string, v: string) => `-[${v}]`],
 ]
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(addEventListener('text-save', (e) => {
