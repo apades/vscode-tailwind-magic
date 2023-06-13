acf736496f6560ff953e7f79284518fd69cad740
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-13 13:49:47 +0800

    chore: 加入了更加严格的环境判断

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import fsp from 'node:fs/promises'
+import fs from 'node:fs'
 import { addEventListener, createBottomBar, getConfiguration, registerCommand } from '@vscode-use/utils'
 import * as vscode from 'vscode'
 
 // todo: 在底部栏增加一个开关来控制是否要启动此插件进行自动处理
 const rules = [
+  ['lh-', 'leading-'],
   [/-\[?\s*(rgba?\([^\)]*\))\s*\]?/g, (_: string, v: string) => `-[${v.replace(/\s*/g, '')}]`],
   [/-\[?\s*(calc\([^\)]*\))\s*\]?/g, (_: string, v: string) => `-[${v.replace(/\s*/g, '')}]`],
   [/-(\#[^\s\"]+)/g, (_: string, v: string) => `-[${v}]`],
@@ -14,9 +15,15 @@ const rules = [
   ['whitespace-nowrap overflow-hidden text-eclipse', 'whitespace-nowrap overflow-hidden text-eclipse'],
 ]
 export function activate(context: vscode.ExtensionContext) {
+  // 只针对当前根目录下有tailwind.config.js | tailwind.config.ts才生效
   const { presets = [], prefix = ['ts', 'js', 'vue', 'tsx', 'jsx', 'svelte'] } = getConfiguration('uno-magic')
+  let isTailwind = false
+  const currentFolder = (vscode.workspace.workspaceFolders as any)[0]
+  if (currentFolder)
+    updateTailwindStatus()
+
   if (presets.length)
-    rules.push(...presets)
+    rules.unshift(...presets)
   let isOpen = true
   // 如果在class或者className中才处理成-[]
   const statusBarItem = createBottomBar({
@@ -40,7 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(addEventListener('text-save', (e) => {
     const url = vscode.window.activeTextEditor!.document.uri.path
-    if (!isOpen)
+    if (!isOpen || !isTailwind)
       return
     // 对文档保存后的内容进行处理
     const text = e.getText()
@@ -51,7 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
       )
     }, text)
     if (newText !== text)
-      fsp.writeFile(url, newText, 'utf-8')
+      fs.promises.writeFile(url, newText, 'utf-8')
   }))
 
   context.subscriptions.push(addEventListener('activeText-change', () =>
@@ -65,6 +72,16 @@ export function activate(context: vscode.ExtensionContext) {
         statusBarItem.show()
     }),
   ))
+  if (!isTailwind) {
+    context.subscriptions.push(addEventListener('file-create', () => {
+      updateTailwindStatus()
+    }))
+  }
+
+  function updateTailwindStatus() {
+    const rootPath = currentFolder.uri.path
+    isTailwind = fs.existsSync(`${rootPath}/tailwind.config.js`) || fs.existsSync(`${rootPath}/tailwind.config.ts`)
+  }
 }
 
 export function deactivate() {
